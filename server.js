d0a8ab405d2892740fc6aeea254314035fd0aaaa
JeraldAng <jeraldedricang@gmail.com>
2019-08-22 22:23:56 +0800

    heroku update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const urlencoder = bodyparser.urlencoded({
 })
 
 mongoose.Promise = global.Promise
-mongoose.connect("mongodb://localhost:27017/PAWSter_Care_db", {
+mongoose.connect("mongodb+srv://admin:<password>@pawstercaredb-iykbj.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 
